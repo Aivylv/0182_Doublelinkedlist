e28811e998f2767c4f84d493a17d4a48aba08d8f
aivylv <lyvialarasati@gmail.com>
2024-05-05 08:56:44 +0700

    menampilkan pesan "Check for the values entered." yg memberitahu user utk memeriksa kembali nilai yg diinput

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -242,5 +242,10 @@ int main()
                 cout << "\nInvalid option" << endl;
                 break;
             }
-
+        }
+        catch (exception& e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+    }
 }
